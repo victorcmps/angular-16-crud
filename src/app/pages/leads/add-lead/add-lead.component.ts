3fb1b3b8fcb715049ccab82e95f2c8388599b536
João Victor Pires Almeida Campos <joao.victor_55@hotmail.com>
2023-10-02 01:50:26 -0300

    feat: add logic to create leads

--- src/app/pages/leads/add-lead/add-lead.component.ts
+++ src/app/pages/leads/add-lead/add-lead.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LeadModel } from 'src/app/models/lead-model';
+import { LeadService } from 'src/app/services/lead.service';
 
 @Component({
   selector: 'app-add-lead',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class AddLeadComponent {
 
+  constructor(private readonly leadService: LeadService, private readonly router: Router) {}
+
+  public readonly createLead = (lead: LeadModel) => {
+    this.leadService.createLead(lead).subscribe({next: () => {
+      this.router.navigate(['/leads']);
+    }})
+  }
 }
