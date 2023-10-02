a07695a83fae2c72e371d95a51f6c798b8b5c065
João Victor Pires Almeida Campos <joao.victor_55@hotmail.com>
2023-10-02 12:53:55 -0300

    feat: add error handlers for api calls

--- src/app/services/lead.service.ts
+++ src/app/services/lead.service.ts
@@ -1,31 +1,63 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { LeadModel } from '../models/lead-model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LeadService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
 
-  public createLead = (lead: LeadModel): Observable<LeadModel> => {
-    return this.http.post<LeadModel>(`leads`, lead);
+  public readonly createLead = (lead: LeadModel): Observable<LeadModel | null> => {
+    return this.http.post<LeadModel>(`leads`, lead).pipe(catchError(this.handleCreateLeadError));
   };
 
-  public getAllLeads = (): Observable<LeadModel[]> => {
-    return this.http.get<LeadModel[]>(`leads`);
+  public readonly getAllLeads = (): Observable<LeadModel[] | null> => {
+    return this.http.get<LeadModel[]>(`leads`).pipe(catchError(this.handleGetAllLeadsError));
   };
 
-  public getLead = (id: string): Observable<LeadModel> => {
-    return this.http.get<LeadModel>(`leads/${id}`);
+  public readonly getLead = (id: string): Observable<LeadModel | null> => {
+    return this.http.get<LeadModel>(`leads/${id}`).pipe(catchError(this.handleGetLeadError));
   };
 
-  public updateLead = (lead: LeadModel, id: string): Observable<LeadModel> => {
-    return this.http.put<LeadModel>(`leads/${id}`, lead);
+  public readonly updateLead = (lead: LeadModel, id: string): Observable<LeadModel | null> => {
+    return this.http.put<LeadModel>(`leads/${id}`, lead).pipe(catchError(this.handleUpdateLeadError));
   };
 
-  public deleteLead = (id: string): Observable<null> => {
-    return this.http.delete<null>(`leads/${id}`);
+  public readonly deleteLead = (id: string): Observable<null> => {
+    return this.http.delete<null>(`leads/${id}`).pipe(catchError(this.handleDeleteLeadError));
   };
+
+  private readonly handleCreateLeadError = (): Observable<null> => {
+    this.snackBar.open('Ocorreu um erro ao criar um novo lead, tente novamente.', 'Dispensar', {
+      duration: 3000
+    });
+    return of(null);
+  }
+
+  private readonly handleGetAllLeadsError = (): Observable<null> => {
+    this.snackBar.open('Ocorreu um erro ao carregar a lista de leads, recarregue a página e tente novamente.');
+    return of(null);
+  }
+
+  private readonly handleGetLeadError = (): Observable<null> => {
+    this.snackBar.open('Ocorreu um erro ao carregar o lead, recarregue a página e tente novamente.');
+    return of(null);
+  }
+
+  private readonly handleUpdateLeadError = (): Observable<null> => {
+    this.snackBar.open('Ocorreu um erro ao atualizar o lead, tente novamente.', 'Dispensar', {
+      duration: 3000
+    });
+    return of(null);
+  }
+
+  private readonly handleDeleteLeadError = (): Observable<null> => {
+    this.snackBar.open('Ocorreu um erro deletar o lead, tente novamente.', 'Dispensar', {
+      duration: 3000
+    });
+    return of(null);
+  }
 }
